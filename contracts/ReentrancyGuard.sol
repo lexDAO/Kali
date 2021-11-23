9e3d5dcece766570a6da98affd70b200629e5925
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-11-23 12:57:03 -0500

    Update ReentrancyGuard.sol
    use internal visibility for modularity

--- contracts/ReentrancyGuard.sol
+++ contracts/ReentrancyGuard.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.8.0;
 /// @notice Gas-optimized reentrancy protection.
 /// @author From RariCapital (https://github.com/Rari-Capital/solmate/blob/main/src/utils/ReentrancyGuard.sol)
 abstract contract ReentrancyGuard {
-    uint256 private reentrancyStatus = 1;
+    uint256 internal reentrancyStatus = 1;
 
     modifier nonReentrant() {
         require(reentrancyStatus == 1, "REENTRANCY");

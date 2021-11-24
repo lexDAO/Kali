e65393fbb650269bb6ec5aa82e9428a9c693fe11
nerderlyne <shivanshityagi@icloud.com>
2021-11-24 22:31:06 +0530

    submit proposal button ui

--- components/ProposeMember.js
+++ components/ProposeMember.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-children-prop */
 import React, { Component } from "react";
 import {
   chakra,
@@ -11,41 +12,59 @@ import {
   NumberInputStepper,
   NumberIncrementStepper,
   NumberDecrementStepper,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 
 class ProposeMember extends Component {
-
   render() {
     return (
       <React.Fragment>
-
-          <form onSubmit={this.props.submitProposal}>
-            <Stack spacing={3}>
-              <Input type="hidden" name="dao" value={this.props.dao['address']} />
-              <Input type="hidden" name="proposalType" value={0} />
-              <Text><b>Details</b></Text>
-              <Textarea name="description" size="lg" placeholder=". . ."/>
-              <Text><b>Recipient</b></Text>
-              <Input name="account" size="lg" placeholder="0x"></Input>
-              <Text><b>Shares</b></Text>
-              <NumberInput name="amount" size="lg" defaultValue={1} min={1}><NumberInputField focusBorderColor="red.200" />
+        <form onSubmit={this.props.submitProposal}>
+          <Stack spacing={3}>
+            <Input type="hidden" name="dao" value={this.props.dao["address"]} />
+            <Input type="hidden" name="proposalType" value={0} />
+            <Text>
+              <b>Details</b>
+            </Text>
+            <Textarea name="description" size="lg" placeholder=". . ." />
+            <Text>
+              <b>Recipient</b>
+            </Text>
+            <Input name="account" size="lg" placeholder="0x"></Input>
+            <Text>
+              <b>Shares</b>
+            </Text>
+            <NumberInput name="amount" size="lg" defaultValue={1} min={1}>
+              <NumberInputField focusBorderColor="red.200" />
               <NumberInputStepper>
-                <NumberIncrementStepper 
+                <NumberIncrementStepper
                   bg="green.200"
                   _active={{ bg: "green.300" }}
-                  children="+"/>
-                <NumberDecrementStepper 
+                  children="+"
+                />
+                <NumberDecrementStepper
                   bg="pink.200"
                   _active={{ bg: "pink.300" }}
                   children="-"
-                 />
-                </NumberInputStepper>
-              </NumberInput>
-              <Input name="payload" type="hidden" value="0x"></Input>
-              <Button type="submit">Submit Proposal</Button>
-            </Stack>
-          </form>
-
+                />
+              </NumberInputStepper>
+            </NumberInput>
+            <Input name="payload" type="hidden" value="0x"></Input>
+            <Button
+              display="flex"
+              justifyContent="center"
+              alignItem="center"
+              colorScheme="kali"
+              size="md"
+              variant="outline"
+              _hover={{
+                color: "kali.500",
+              }}
+              type="submit"
+            >
+              Submit Proposal
+            </Button>
+          </Stack>
+        </form>
       </React.Fragment>
     );
   }

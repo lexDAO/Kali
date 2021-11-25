46e4c98f3c12704736b47205638a6801aa2294a0
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-11-25 03:58:50 -0500

    Update factory.js
    update factory

--- eth/factory.js
+++ eth/factory.js
@@ -3,7 +3,7 @@ const abi = require('../abi/KaliDAOfactory.json');
 
 const factory = new web3.eth.Contract(
   abi,
-  "0xBfe01d81A64e6537a53D3825699A719f6b9C01b8"
+  "0x0D49bc598856cfE3eaA99403D1720E203fa466d8"
 );
 
 export default factory;

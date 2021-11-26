c56e7d0f772f1464dd45af205d80e25ee2333f37
jordanteague <ejteague@gmail.com>
2021-11-26 08:56:55 -0500

    updated deployment addresses

--- eth/factory.js
+++ eth/factory.js
@@ -1,14 +1,12 @@
 import web3 from "./web3";
 const abi = require('../abi/KaliDAOfactory.json');
 
-const polygon = "0x33c8268e4A0ED135DDAc52Ea4CCE67F10e9605Cb";
-const rinkeby = "0x411a1EBd260eB0E4cB534Cb3f4974B7952ebD4e9";
+const polygon = "0x3F02b702b2CEF418dfa7Cc9AB67086263bf0A457";
+const rinkeby = "0xD219594C6913705c3e8B2A7b3442729f29b16E2b";
 
 const factory = new web3.eth.Contract(
   abi,
   rinkeby
 );
-
-// polygon: 0x33c8268e4A0ED135DDAc52Ea4CCE67F10e9605Cb
 
 export default factory;

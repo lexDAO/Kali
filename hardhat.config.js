4dd35148ead10289819cb3a7704fa5b4bf33fcf7
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-11-27 17:47:33 -0500

    Update hardhat.config.js
    add optimizer and other features to help run tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+//require("hardhat-gas-reporter");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,5 +18,57 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.10",
+  defaultNetwork: "hardhat",
+  gasReporter: {
+    currency: "USD",
+    enabled: true,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    alice: {
+      default: 1,
+    },
+    bob: {
+      default: 2,
+    },
+    carol: {
+      default: 3,
+    },
+  },
+  networks: {
+    localhost: {
+      live: false,
+      saveDeployments: true,
+      tags: ["local"],
+    },
+    hardhat: {
+      forking: {
+        enabled: true,
+        url: "https://eth-mainnet.alchemyapi.io/v2/zjx8baDblg7pbUjvc4zuRARLT28Ft2PC",
+        blockNumber: 12886725
+      },
+      allowUnlimitedContractSize: true,
+      live: false,
+      saveDeployments: true,
+      tags: ["test", "local"],
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.10",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 6999,
+          },
+        },
+      }
+    ],
+  },
+  mocha: {
+    timeout: 200000
+  },
 };

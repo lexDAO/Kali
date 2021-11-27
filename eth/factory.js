c3e7e220ec3344e9eb7f10c1b935481a38e5a87e
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-11-27 01:21:40 -0500

    Update factory.js
    update rink factory

--- eth/factory.js
+++ eth/factory.js
@@ -2,7 +2,7 @@ import web3 from "./web3";
 const abi = require('../abi/KaliDAOfactory.json');
 
 const polygon = "0x3F02b702b2CEF418dfa7Cc9AB67086263bf0A457";
-const rinkeby = "0x7c5B08e18FD1864b44b23372820680a45BB7Ff3A";
+const rinkeby = "0xBc87B1A40796070d29e7aC8d08fB61D2A7685A27";
 
 const factory = new web3.eth.Contract(
   abi,

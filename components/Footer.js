32ea513b41f79cf63f9ed57880ac8b16fed46711
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-11-28 04:18:58 -0500

    Update Footer.js
    add url to twitter

--- components/Footer.js
+++ components/Footer.js
@@ -2,6 +2,6 @@ import { Text, Flex } from "@chakra-ui/react";
 
 export default function Footer() {
   return (
-    <Flex m={30}><Text fontSize="xs">brought to you by LexDAO</Text></Flex>
+    <Flex m={30}><Text fontSize="xs">brought to you by <a href= "https://twitter.com/lex_DAO"><i>LexDAO</i></a></Text></Flex>
   )
 }

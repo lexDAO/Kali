a12672532575cac4a2eaad4b15f94b7345f79541
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-11-28 01:14:08 -0500

    Update factory.js
    update factories with higher optimizer runs to match arbitrum deployment

--- eth/factory.js
+++ eth/factory.js
@@ -2,8 +2,8 @@ import web3 from "./web3";
 const abi = require('../abi/KaliDAOfactory.json');
 
 const arbitrum = "0xd53B46aE3781904F1f61CF38Fd9d4F47A7e9242B";
-const polygon = "0xd40223f63f8D44bE51CDE912aafc30dc340dB438";
-const rinkeby = "0xed35f7A980987AeC418C70bb4e01CA4799EC6EFd";
+const polygon = "0x582eAF6a83E55d60615A5FfB80913bE5c1724c41";
+const rinkeby = "0x6106375F8549fD1a688956F7070aa8bA3fdF51b2";
 
 const factory = new web3.eth.Contract(
   abi,

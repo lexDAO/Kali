c4ac5f69aad71d9b357adfbfe202b339a7aa4a7c
nerderlyne <shivanshityagi@icloud.com>
2021-11-28 16:53:12 +0530

    footer socials

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,56 @@
-import { Text, Flex } from "@chakra-ui/react";
+import {
+  Text,
+  Container,
+  Stack,
+  Button,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { BsTwitter, BsGithub } from "react-icons/bs";
 
 export default function Footer() {
   return (
-    <Flex m={30}><Text fontSize="xs">brought to you by <a href= "https://twitter.com/lex_DAO"><i>LexDAO</i></a></Text></Flex>
-  )
+    <Container
+      as={Stack}
+      mt={5}
+      maxW={"5xl"}
+      py={4}
+      direction={{ base: "column", md: "row" }}
+      spacing={4}
+      justify={{ base: "center", md: "space-between" }}
+      align={{ base: "center", md: "center" }}
+    >
+      <Text fontSize="xs">
+        Brought to you by{" "}
+        <a href="https://twitter.com/lex_DAO">
+          <i>LexDAO</i>
+        </a>
+      </Text>
+      <Stack direction={"row"} spacing={4}>
+        <Button
+          rounded={"full"}
+          as={"a"}
+          href={"https://twitter.com/_KaliDAO"}
+          target="_blank"
+          display={"inline-flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          transition={"background 0.3s ease"}
+        >
+          <BsTwitter />
+        </Button>
+        <Button
+          rounded={"full"}
+          as={"a"}
+          href={"https://github.com/lexDAO/Kali"}
+          target="_blank"
+          display={"inline-flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          transition={"background 0.3s ease"}
+        >
+          <BsGithub />
+        </Button>
+      </Stack>
+    </Container>
+  );
 }

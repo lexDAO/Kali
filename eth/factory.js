059b78e35023e36cad83d2d1c771ceb275e66e2c
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-11-28 01:07:51 -0500

    Update factory.js
    add arbitrum

--- eth/factory.js
+++ eth/factory.js
@@ -1,6 +1,7 @@
 import web3 from "./web3";
 const abi = require('../abi/KaliDAOfactory.json');
 
+const arbitrum = "0xd53B46aE3781904F1f61CF38Fd9d4F47A7e9242B";
 const polygon = "0xd40223f63f8D44bE51CDE912aafc30dc340dB438";
 const rinkeby = "0xed35f7A980987AeC418C70bb4e01CA4799EC6EFd";
 

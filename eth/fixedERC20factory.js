25501ddc53558561527a5a9e9d49381d6808bba6
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-11-28 21:50:34 -0500

    Update fixedERC20factory.js
    nit addr

--- eth/fixedERC20factory.js
+++ eth/fixedERC20factory.js
@@ -1,9 +1,9 @@
 import web3 from "./web3";
 const abi = require('../abi/FixedERC20factory.json');
 
-const arbitrum = "0x2985bDB7eC8f971126B6eBCd9B65662D22a59c6F";
-const polygon = "0x5fd571Dca13E1fd2CEff343B574D76A33c8B20C6";
-const rinkeby = "0x70EfE1857d508368fa9645345ceAC87f6dDB4229";
+const arbitrum = "0x0D6aDcBb5b8426931ca0Ce8E41eBaE4D87CE354E";
+const polygon = "0x185009425978954512C23FCD94e090A5b6847eeE";
+const rinkeby = "0x0D6aDcBb5b8426931ca0Ce8E41eBaE4D87CE354E";
 
 const factory = new web3.eth.Contract(
   abi,

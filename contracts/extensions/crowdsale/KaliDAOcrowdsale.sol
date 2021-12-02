41256c6c6e6a4f38b4edbd5e7ffb5522216c7efc
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-12-01 22:36:37 -0500

    Update KaliDAOcrowdsale.sol
    make modular to support multiple daos

--- contracts/extensions/crowdsale/KaliDAOcrowdsale.sol
+++ contracts/extensions/crowdsale/KaliDAOcrowdsale.sol
@@ -2,72 +2,58 @@
 
 pragma solidity >=0.8.0;
 
+import '../../libraries/SafeTransferLib.sol';
 import '../../ReentrancyGuard.sol';
 
-/// @notice Crowdsale contract that receives ETH or tokens to mint DAO tokens.
+/// @notice Crowdsale contract that receives ETH or tokens to mint registered DAO tokens.
 contract KaliDAOcrowdsale is ReentrancyGuard {
-    address public immutable dao;
-    
-    address public immutable purchaseToken;
+    mapping(address => Crowdsale) public crowdsales;
 
-    bool public immutable ethBased;
-    
-    uint8 public immutable purchaseMultiplier;
-
-    uint256 public immutable purchaseLimit;
-
-    uint256 public amountPurchased;
-    
-    constructor(address dao_, address purchaseToken_, bool ethBased_, uint8 purchaseMultiplier_, uint256 purchaseLimit_) {
-        dao = dao_;
-        
-        purchaseToken = purchaseToken_;
-
-        ethBased = ethBased_;
-        
-        purchaseMultiplier = purchaseMultiplier_;
-
-        purchaseLimit = purchaseLimit_;
+    struct Crowdsale {
+        address purchaseToken;
+        uint8 purchaseMultiplier;
+        uint112 purchaseLimit;
+        uint112 amountPurchased;
     }
-    
-    function extensionCall(address account, uint256 amount) public payable nonReentrant virtual returns (uint256 amountOut) {
-        require(msg.sender == dao, 'NOT_DAO');
+
+    function setExtension(bytes calldata extensionData_) public nonReentrant virtual {
+        (address dao_, address purchaseToken_, uint8 purchaseMultiplier_, uint112 purchaseLimit_) 
+            = abi.decode(extensionData_, (address, address, uint8, uint112));
         
-        if (ethBased) {
-            amountOut = msg.value * purchaseMultiplier;
+        require(purchaseMultiplier_ > 0, "NULL_MULTIPLIER"); 
 
-            require(amountPurchased + amountOut <= purchaseLimit, "PURCHASE_LIMIT");
-            
+        require(crowdsales[dao_].purchaseMultiplier > 0 || dao_ == msg.sender, 
+            "INITIALIZED_OR_NOT_DAO"); 
+
+        crowdsales[dao_] = Crowdsale({
+            purchaseToken: purchaseToken_,
+            purchaseMultiplier: purchaseMultiplier_,
+            purchaseLimit: purchaseLimit_,
+            amountPurchased: 0
+        });
+    }
+
+    function callExtension(address account, uint256 amount) public payable nonReentrant virtual returns (uint256 amountOut) {
+        Crowdsale storage sale = crowdsales[msg.sender];
+
+        if (sale.purchaseToken == address(0)) {
+            amountOut = msg.value * sale.purchaseMultiplier;
+
+            require(sale.amountPurchased + amountOut <= sale.purchaseLimit, "PURCHASE_LIMIT");
+
             // send ETH to `dao`
-            _safeTransferETH(msg.sender, msg.value);
+            SafeTransferLib.safeTransferETH(msg.sender, msg.value);
 
-            amountPurchased += amountOut;
+            sale.amountPurchased += uint112(amountOut);
         } else {
             // send tokens to `dao`
-            _safeTransferFrom(purchaseToken, account, msg.sender, amount);
-            
-            amountOut = amount * purchaseMultiplier;
+            SafeTransferLib.safeTransferFrom(sale.purchaseToken, account, msg.sender, amount);
 
-            require(amountPurchased + amountOut <= purchaseLimit, "PURCHASE_LIMIT");
+            amountOut = amount * sale.purchaseMultiplier;
 
-            amountPurchased += amountOut;
+            require(sale.amountPurchased + amountOut <= sale.purchaseLimit, "PURCHASE_LIMIT");
+
+            sale.amountPurchased += uint112(amountOut);
         }
-    }
-    
-    /*///////////////////////////////////////////////////////////////
-                            TRANSFER HELPERS
-    //////////////////////////////////////////////////////////////*/
-
-    function _safeTransferETH(address to, uint256 value) internal virtual {
-        (bool success, ) = to.call{value: value}("");
-        
-        require(success, "ETH_TRANSFER_FAILED");
-    }
-    
-    function _safeTransferFrom(address token, address from, address to, uint256 amount) internal virtual {
-        // transferFrom(address,address,uint256)
-        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, amount));
-        
-        require(success && (data.length == 0 || abi.decode(data, (bool))), "PULL_FAILED");
     }
 }

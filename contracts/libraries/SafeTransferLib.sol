92a170282dec26d7a2c59b148d1de07b978cc5bc
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-12-02 01:38:26 -0500

    Update SafeTransferLib.sol
    kali style

--- contracts/libraries/SafeTransferLib.sol
+++ contracts/libraries/SafeTransferLib.sol
@@ -15,7 +15,7 @@ library SafeTransferLib {
         bool callStatus;
 
         assembly {
-            // Transfer the ETH and store if it succeeded or not.
+            // transfer the ETH and store if it succeeded or not
             callStatus := call(gas(), to, amount, 0, 0, 0, 0)
         }
 
@@ -35,17 +35,20 @@ library SafeTransferLib {
         bool callStatus;
 
         assembly {
-            // Get a pointer to some free memory.
+            // get a pointer to some free memory
             let freeMemoryPointer := mload(0x40)
 
-            // Write the abi-encoded calldata to memory piece by piece:
-            mstore(freeMemoryPointer, 0x23b872dd00000000000000000000000000000000000000000000000000000000) // Begin with the function selector.
-            mstore(add(freeMemoryPointer, 4), and(from, 0xffffffffffffffffffffffffffffffffffffffff)) // Mask and append the "from" argument.
-            mstore(add(freeMemoryPointer, 36), and(to, 0xffffffffffffffffffffffffffffffffffffffff)) // Mask and append the "to" argument.
-            mstore(add(freeMemoryPointer, 68), amount) // Finally append the "amount" argument. No mask as it's a full 32 byte value.
+            // write the abi-encoded calldata to memory piece by piece:
+            mstore(freeMemoryPointer, 0x23b872dd00000000000000000000000000000000000000000000000000000000) // begin with the function selector
+            
+            mstore(add(freeMemoryPointer, 4), and(from, 0xffffffffffffffffffffffffffffffffffffffff)) // mask and append the "from" argument
+            
+            mstore(add(freeMemoryPointer, 36), and(to, 0xffffffffffffffffffffffffffffffffffffffff)) // mask and append the "to" argument
+            
+            mstore(add(freeMemoryPointer, 68), amount) // finally append the "amount" argument - no mask as it's a full 32 byte value
 
-            // Call the token and store if it succeeded or not.
-            // We use 100 because the calldata length is 4 + 32 * 3.
+            // call the token and store if it succeeded or not
+            // we use 100 because the calldata length is 4 + 32 * 3
             callStatus := call(gas(), token, 0, freeMemoryPointer, 100, 0, 0)
         }
 
@@ -60,16 +63,18 @@ library SafeTransferLib {
         bool callStatus;
 
         assembly {
-            // Get a pointer to some free memory.
+            // get a pointer to some free memory
             let freeMemoryPointer := mload(0x40)
 
-            // Write the abi-encoded calldata to memory piece by piece:
-            mstore(freeMemoryPointer, 0xa9059cbb00000000000000000000000000000000000000000000000000000000) // Begin with the function selector.
-            mstore(add(freeMemoryPointer, 4), and(to, 0xffffffffffffffffffffffffffffffffffffffff)) // Mask and append the "to" argument.
-            mstore(add(freeMemoryPointer, 36), amount) // Finally append the "amount" argument. No mask as it's a full 32 byte value.
+            // write the abi-encoded calldata to memory piece by piece:
+            mstore(freeMemoryPointer, 0xa9059cbb00000000000000000000000000000000000000000000000000000000) // begin with the function selector
+            
+            mstore(add(freeMemoryPointer, 4), and(to, 0xffffffffffffffffffffffffffffffffffffffff)) // mask and append the "to" argument
+            
+            mstore(add(freeMemoryPointer, 36), amount) // finally append the "amount" argument - no mask as it's a full 32 byte value
 
-            // Call the token and store if it succeeded or not.
-            // We use 68 because the calldata length is 4 + 32 * 2.
+            // call the token and store if it succeeded or not
+            // we use 68 because the calldata length is 4 + 32 * 2
             callStatus := call(gas(), token, 0, freeMemoryPointer, 68, 0, 0)
         }
 
@@ -80,34 +85,35 @@ library SafeTransferLib {
                          INTERNAL HELPER LOGIC
     //////////////////////////////////////////////////////////////*/
 
-    function didLastOptionalReturnCallSucceed(bool callStatus) private pure returns (bool success) {
+    function didLastOptionalReturnCallSucceed(bool callStatus) internal pure returns (bool success) {
         assembly {
-            // Get how many bytes the call returned.
+            // get how many bytes the call returned
             let returnDataSize := returndatasize()
 
-            // If the call reverted:
+            // if the call reverted:
             if iszero(callStatus) {
-                // Copy the revert message into memory.
+                // copy the revert message into memory
                 returndatacopy(0, 0, returnDataSize)
 
-                // Revert with the same message.
+                // revert with the same message
                 revert(0, returnDataSize)
             }
 
             switch returnDataSize
+            
             case 32 {
-                // Copy the return data into memory.
+                // copy the return data into memory
                 returndatacopy(0, 0, returnDataSize)
 
-                // Set success to whether it returned true.
+                // set success to whether it returned true
                 success := iszero(iszero(mload(0)))
             }
             case 0 {
-                // There was no return data.
+                // there was no return data
                 success := 1
             }
             default {
-                // It returned some malformed input.
+                // it returned some malformed input
                 success := 0
             }
         }

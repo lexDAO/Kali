d1182cce8afcd11f9022291c3573b07d9d101c1c
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-12-04 14:45:29 -0500

    Update factory.js
    new deployment

--- eth/factory.js
+++ eth/factory.js
@@ -3,7 +3,7 @@ const abi = require('../abi/KaliDAOfactory.json');
 
 const arbitrum = "0xd53B46aE3781904F1f61CF38Fd9d4F47A7e9242B";
 const polygon = "0x582eAF6a83E55d60615A5FfB80913bE5c1724c41";
-const rinkeby = "0x6106375F8549fD1a688956F7070aa8bA3fdF51b2";
+const rinkeby = "0xD2C17f25851D3B1b5E245b6a4484a84607Cb80B4";
 
 const factory = new web3.eth.Contract(
   abi,

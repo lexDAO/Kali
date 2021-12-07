72e59aac9567c3d2d22ce7445f0e49d699a3e8fd
Jordan Teague <ejteague@gmail.com>
2021-12-06 19:13:11 -0500

    web3modal again

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import AppContext from '../context/AppContext';
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import Web3Modal from "web3modal";
+import Web3 from 'web3';
 import { useState, useEffect } from 'react';
-import web3m from '../utils/web3modal';
+//import web3m from '../utils/web3modal';
 import web3Infura from '../utils/infura';
 import theme from '../styles/theme';
 
@@ -34,8 +37,23 @@ function MyApp({ Component, pageProps }) {
   }, []);
 
   const connect = async() => {
+    const providerOptions = {
+      walletconnect: {
+        package: WalletConnectProvider, // required
+        options: {
+          infuraId: "26e178ea568e492983f2431ad6a31e74" // required
+        }
+      }
+    };
+    // We are in the browser and metamask is running.
+    const web3Modal = new Web3Modal({
+      providerOptions
+    });
+
+    const provider = await web3Modal.connect();
+
+    setWeb3(new Web3(provider));
     console.log("pressed")
-    setWeb3(web3m);
     let accounts = await web3.eth.getAccounts();
     setAccount(accounts[0]);
     setChainId(await web3.eth.getChainId());

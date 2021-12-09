072c8c30b5e519104dd71ddc32b7039ca6b78f54
nerderlyne <shivanshityagi@icloud.com>
2021-12-09 23:27:38 +0530

    render pls

--- components/Factory.js
+++ components/Factory.js
@@ -1,36 +1,22 @@
 import React, { Component, useContext } from "react";
-import AppContext from '../context/AppContext';
+import AppContext from "../context/AppContext";
 import Router, { useRouter } from "next/router";
-import {
-  Flex,
-  Input,
-  Button,
-  Text,
-  Textarea,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-  NumberIncrementStepper,
-  NumberDecrementStepper,
-  Select,
-} from "@chakra-ui/react";
+import { Text, VStack, HStack, Box, Input, Button } from "@chakra-ui/react";
 import FlexGradient from "./FlexGradient";
-import { Formik, Form } from "formik";
+import { Formik, Form, Field, FieldArray } from "formik";
 import * as Yup from "yup";
 import FormikControl from "./form/FormikControl.js";
-const abi = require('../abi/KaliDAOfactory.json');
-import { factory_rinkeby } from "../utils/addresses"
+const abi = require("../abi/KaliDAOfactory.json");
+import { factory_rinkeby } from "../utils/addresses";
 
 export default function Factory(props) {
   const value = useContext(AppContext);
   const { web3, account, chainId, loading } = value.state;
-  const factory = new web3.eth.Contract(
-    abi,
-    factory_rinkeby
-  );
+  const factory = new web3.eth.Contract(abi, factory_rinkeby);
 
   const handleFactorySubmit = async (values) => {
-      value.setLoading(true);
+    console.log("form:", values);
+    /*      value.setLoading(true);
 
       const govSettings = "0,60,0,0,0,0,0,0,0,0,0,0";
       const extensions = new Array(0);
@@ -126,103 +112,127 @@ export default function Factory(props) {
       }
 
       value.setLoading(false)
-    };
+*/
+  };
 
-    const initialValues = {
-      name: "",
-      symbol: "",
-      docs: "",
-      voters: "",
-      shares: "",
-      votingPeriodUnit: "",
-      votingPeriod: 1,
-    };
+  const initialForm = {
+    name: "",
+    symbol: "",
+    docs: "",
+    founders: [{ voter: "", share: "" }],
+    votingPeriodUnit: "",
+    votingPeriod: 1,
+  };
 
-    const validationSchema = Yup.object({
-      name: Yup.string().required("Required"),
-      symbol: Yup.string().required("Required"),
-      voters: Yup.string().required("Required"),
-      shares: Yup.string().required("Required"),
-      docs: Yup.string().required("Required"),
-      votingPeriodUnit: Yup.string().required("Required"),
-      votingPeriod: Yup.number().required("Required"),
-    });
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Required"),
+    symbol: Yup.string().required("Required"),
+    founders: Yup.array(
+      Yup.object({
+        voter: Yup.string().required("Required"),
+        share: Yup.string().required("Required"),
+      })
+    ),
+    docs: Yup.string().required("Required"),
+    votingPeriodUnit: Yup.string().required("Required"),
+    votingPeriod: Yup.number().required("Required"),
+  });
 
-    const optionsDocs = [
-      { key: "Select Document", value: "" },
-      { key: "Code of Conduct", value: "COC" },
-      { key: "UNA", value: "UNA" },
-      { key: "LLC", value: "LLC" },
-    ];
+  const optionsDocs = [
+    { key: "Select Document", value: "" },
+    { key: "Code of Conduct", value: "COC" },
+    { key: "UNA", value: "UNA" },
+    { key: "LLC", value: "LLC" },
+  ];
 
-    const optionsVotingPeriod = [
-      { key: "Select Voting Period", value: "" },
-      { key: "Minutes", value: "minutes" },
-      { key: "Hours", value: "hours" },
-      { key: "Days", value: "days" },
-    ];
+  const optionsVotingPeriod = [
+    { key: "Select Voting Period", value: "" },
+    { key: "Minutes", value: "minutes" },
+    { key: "Hours", value: "hours" },
+    { key: "Days", value: "days" },
+  ];
 
-    return (
-      <FlexGradient>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={handleFactorySubmit}
-        >
-          {() => (
-            <Form>
-              <FormikControl
-                control="input"
-                type="text"
-                label="Name"
-                name="name"
-                placeholder="KaliDAO"
-              />
-              <FormikControl
-                control="input"
-                type="text"
-                label="Symbol"
-                name="symbol"
-                placeholder="KALI"
-              />
-              <FormikControl
-                control="select"
-                label="Document"
-                name="docs"
-                options={optionsDocs}
-              />
-              <FormikControl
-                control="textarea"
-                type="text"
-                label="Founders"
-                name="voters"
-                placeholder="Enter ETH address or ENS and separate them by a comma like this - 'aaa.eth,bbb.eth'"
-              />
-              <FormikControl
-                control="textarea"
-                type="text"
-                label="Shares"
-                name="shares"
-                placeholder="1,2,3"
-              />
-              <FormikControl
-                control="number-input"
-                label="Voting Period"
-                name="votingPeriod"
-                defaultValue={3}
-                min={1}
-              />
-              <FormikControl
-                control="select"
-                name="votingPeriodUnit"
-                label="Voting Period Unit"
-                options={optionsVotingPeriod}
-              />
-              <br />
-              <Button type="submit">Summon</Button>
-            </Form>
-          )}
-        </Formik>
-      </FlexGradient>
-    );
-  }
+  return (
+    <FlexGradient>
+      <Formik
+        initialValues={initialForm}
+        validationSchema={validationSchema}
+        onSubmit={handleFactorySubmit}
+      >
+        {(values, errors) => (
+          <Form>
+            <FormikControl
+              control="input"
+              type="text"
+              label="Name"
+              name="name"
+              placeholder="KaliDAO"
+            />
+            <FormikControl
+              control="input"
+              type="text"
+              label="Symbol"
+              name="symbol"
+              placeholder="KALI"
+            />
+            <FormikControl
+              control="select"
+              label="Document"
+              name="docs"
+              options={optionsDocs}
+            />
+            <FieldArray name="founders"
+              render={({ push, remove }) => (
+                <>
+                  {values.founders > 0 && 
+                    values.founders.map((_, index) => (
+                      <>
+                        <FormikControl
+                          control="input"
+                          type="text"
+                          label="Founder"
+                          name={`founders.${index}.voter`}
+                          placeholder="Enter ETH address or ENS"
+                        />
+                        <FormikControl
+                          control="input"
+                          type="text"
+                          label="Share"
+                          name={`founders.${index}.share`}
+                          placeholder="1"
+                        />
+                        <Button onClick={() => remove(index)}>Delete</Button>
+                      </>
+                    ))}
+                  
+                    <Button onClick={() => push({ voter: "", share: "" })}>
+                      Add Founder
+                    </Button>
+        
+                </>
+              )}
+            />
+            <FormikControl
+              control="number-input"
+              label="Voting Period"
+              name="votingPeriod"
+              defaultValue={3}
+              min={1}
+            />
+            <FormikControl
+              control="select"
+              name="votingPeriodUnit"
+              label="Voting Period Unit"
+              options={optionsVotingPeriod}
+            />
+            <br />
+            <Button type="submit" isFullWidth>
+              Summon
+            </Button>
+            <pre>{JSON.stringify({ values, errors }, null, 4)}</pre>
+          </Form>
+        )}
+      </Formik>
+    </FlexGradient>
+  );
+}

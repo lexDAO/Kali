059150537789170e060b97d8462186980424dd19
Jordan Teague <ejteague@gmail.com>
2021-12-10 00:10:00 -0500

    Update addresses.js

--- utils/addresses.js
+++ utils/addresses.js
@@ -1,4 +1,1 @@
-// factory addresses
-export const factory_arbitrum = "0xd53B46aE3781904F1f61CF38Fd9d4F47A7e9242B";
-export const factory_polygon = "0x582eAF6a83E55d60615A5FfB80913bE5c1724c41";
-export const factory_rinkeby = "0x4E2df5aD942fAfD27a68FA793C6A6494C9bE998E";
+export const factory_rinkeby = "0xF300cDD2943b645A4546A1c0633fF1EB5F457973";

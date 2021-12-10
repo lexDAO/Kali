29fe31a692bee4b3a94a1bfa5de463c5ad5d5d98
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-12-10 00:10:52 -0500

    Update HomeTile.js
    simplify

--- components/home/HomeTile.js
+++ components/home/HomeTile.js
@@ -9,14 +9,6 @@ export default function HomeTile() {
   return(
     <FlexGradient>
       <Stack spacing={5} p={5} alignItems="center">
-        <Text
-          fontSize="4xl"
-          color="kali.700"
-          fontWeight="bold"
-          letterSpacing="wide"
-        >
-          KaliCo
-        </Text>
         <Text fontSize="xl">
           Launch and automate your business with DAO magick.
         </Text>

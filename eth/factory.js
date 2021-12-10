f558b1ebc3fcef0169a2f94df40dbe53a7778fc2
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-12-09 19:53:05 -0500

    Update factory.js
    nit factory

--- eth/factory.js
+++ eth/factory.js
@@ -4,7 +4,7 @@ const abi = require('../abi/KaliDAOfactory.json');
 
 const arbitrum = "0xd53B46aE3781904F1f61CF38Fd9d4F47A7e9242B";
 const polygon = "0x582eAF6a83E55d60615A5FfB80913bE5c1724c41";
-const rinkeby = "0xb99dd4FEcbC2D4474BF8a49d8dDE86C18ac5D696";
+const rinkeby = "0x98750d8fc6d7e584E9f715ca980D73eAd8433B18";
 
 export default function makeFactory() {
   const value = useContext(AppContext);

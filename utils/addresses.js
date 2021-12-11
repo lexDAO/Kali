a90e4c1a8df3c165e2ed04b3be59b7cb16d07058
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-12-10 22:45:47 -0500

    Update addresses.js
    bump factory

--- utils/addresses.js
+++ utils/addresses.js
@@ -1,4 +1,4 @@
 // factory addresses
 export const factory_arbitrum = "0xd53B46aE3781904F1f61CF38Fd9d4F47A7e9242B";
 export const factory_polygon = "0x582eAF6a83E55d60615A5FfB80913bE5c1724c41";
-export const factory_rinkeby = "0x4867aE31fE6a309D18eddeeE4Df41228ae841bdd";
+export const factory_rinkeby = "0x38c0bFD8bba8588087c1325575cA82Bb09FDd013";

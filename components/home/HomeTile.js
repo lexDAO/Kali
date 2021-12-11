b34424786a25f61500a25159fa7a9278d277036b
Jordan Teague <ejteague@gmail.com>
2021-12-11 11:02:13 -0500

    mobile

--- components/home/HomeTile.js
+++ components/home/HomeTile.js
@@ -4,13 +4,19 @@ import {
   Stack,
 } from "@chakra-ui/react";
 import FlexGradient from '../elements/FlexGradient';
+import {BrowserView, MobileView, isBrowser} from 'react-device-detect';
 
 export default function HomeTile() {
   return(
     <FlexGradient>
       <Stack spacing={5} p={5} alignItems="center">
         <Text fontSize="xl">
-          Launch and automate your business with real DAO magick
+          <BrowserView>
+          Launch and automate your business with real DAO magick BrowserView
+          </BrowserView>
+          <MobileView>
+          MobileView
+          </MobileView>
         </Text>
       </Stack>
     </FlexGradient>

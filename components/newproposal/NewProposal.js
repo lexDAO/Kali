218e9c28c3e792e9497a76893589db643a1a88b5
Jordan Teague <ejteague@gmail.com>
2021-12-11 18:08:58 -0500

    added mobile vs. desktop views for proposal creation page

--- components/newproposal/NewProposal.js
+++ components/newproposal/NewProposal.js
@@ -1,55 +1,75 @@
 import { useState, useContext, useEffect } from 'react';
 import AppContext from '../../context/AppContext';
 import {
-  Input,
-  Button,
   Select,
   Text,
-  Textarea
+  Box,
+  Grid,
+  Button
 } from "@chakra-ui/react";
 import FlexOutline from "../elements/FlexOutline";
 import SendShares from "./SendShares";
 import RemoveMember from "./RemoveMember";
 import SendToken from "./SendToken";
 import ContractCall from "./ContractCall";
+import {BrowserView, MobileView} from 'react-device-detect';
 
 export default function NewProposal(props) {
-  const [menuItem, setMenuItem] = useState(999); // arbitrary number where no proposal type is selected. if changed, must change below, too
-
+  const [menuItem, setMenuItem] = useState(0); // arbitrary number where no proposal type is selected. if changed, must change below, too
   const value = useContext(AppContext);
   const { web3, loading, account, abi, address } = value.state;
   const balances = props.balances;
 
-  const updateMenuItem = (e) => {
+  const ProposalTile = (props) => {
+    return(
+        <Box backgroundColor="lightgrey" p={5} m={5}>
+          {props.children}
+          <Button onClick={() => setMenuItem(props.id)}>{props.text}</Button>
+        </Box>
+      )
+    }
+
+    const updateMenuItem = (e) => {
     let newValue = e.target.value;
     setMenuItem(newValue);
   };
 
-  const test = 0;
+  const proposalTypes = ["Send Shares", "Send a Token", "Call a Contract", "Remove Member"];
 
   return(
     <>
-      <form>
-        <FlexOutline>
-          <Select
-            name="menuItem" // will have to convert to proposalType corresponding with smart contract enums
-            onChange={updateMenuItem}
-            color="kali.800"
-            bg="kali.900"
-            opacity="0.9"
-          >
-            <option value="999">Select a proposal type</option>
-            <option value="0">Send Shares</option>
-            <option value="1">Send a Token</option>
-            <option value="2">Call Contract</option>
-            <option value="3">Remove Member</option>
+      <MobileView>
+        <form>
+          <FlexOutline>
+            <Select
+              name="menuItem" // will have to convert to proposalType corresponding with smart contract enums
+              onChange={updateMenuItem}
+              color="kali.800"
+              bg="kali.900"
+              opacity="0.9"
+            >
+              <option value="999">Select a proposal type</option>
+              {
+                proposalTypes.map((p, index) => (
+                  <option value={index}>{p}</option>
+                ))
+              }
+            </Select>
+          </FlexOutline>
+        </form>
+      </MobileView>
 
-          </Select>
+      <BrowserView>
+      <Grid templateColumns={{sm: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)', lg: 'repeat(4, 1fr)'}}>
 
-        </FlexOutline>
+        {
+          proposalTypes.map((p, index) => (
+            <ProposalTile id={index} text={p} />
+          ))
+        }
 
-      </form>
-
+      </Grid>
+      </BrowserView>
       {menuItem==0 ? <SendShares /> : null }
       {menuItem==1 ? <SendToken /> : null }
       {menuItem==2 ? <ContractCall /> : null }
--- components/structure/ActionMenu.js
+++ components/structure/ActionMenu.js
@@ -10,17 +10,33 @@ import {
   IconButton
 } from "@chakra-ui/react";
 import { BiRefresh } from "react-icons/bi";
+
+const ActionButton = (props) => {
+  return(
+    <Button
+      onClick={props.onClick}
+      size="sm"
+      backgroundColor={props.backgroundColor}
+      border="0px"
+    >
+      {props.children}
+    </Button>
+  );
+}
+const active = "red";
+const inactive = "white";
+
 export default function ActionMenu(props) {
   const value = useContext(AppContext);
   const { web3, loading, abi, reload, visibleView } = value.state;
 
   return(
     <Center>
-      <HStack p={5}>
-        <Button size="sm" onClick={() => value.setVisibleView(1)}>Proposals</Button>
-        <Button size="sm" onClick={() => value.setVisibleView(2)}>New Proposal</Button>
-        <Button size="sm" onClick={() => value.setVisibleView(3)}>DAO Info</Button>
-        <IconButton size="sm" aria-label='refresh' icon={<BiRefresh />} onClick={value.reloadButton} />
+      <HStack p={5} backgroundColor="lightgrey" rounded="md" m={3} p={3}>
+        <ActionButton onClick={() => value.setVisibleView(1)} backgroundColor={visibleView==1 ? active : inactive }>Proposals</ActionButton>
+        <ActionButton onClick={() => value.setVisibleView(2)} backgroundColor={visibleView==2 ? active : inactive }>New Proposal</ActionButton>
+        <ActionButton onClick={() => value.setVisibleView(3)} backgroundColor={visibleView==3 ? active : inactive }>DAO Info</ActionButton>
+        <IconButton border="0px" size="sm" aria-label='refresh' icon={<BiRefresh />} onClick={value.reloadButton} />
       </HStack>
     </Center>
   )

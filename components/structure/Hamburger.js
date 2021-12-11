86489588f3b42627ee39937453a7710bf46e55d5
nerderlyne <shivanshityagi@icloud.com>
2021-12-11 16:08:43 +0530

    menu setup

--- components/structure/Hamburger.js
+++ components/structure/Hamburger.js
@@ -1,13 +1,37 @@
-import { IconButton } from "@chakra-ui/react";
-import { AiOutlineEllipsis } from "react-icons/ai";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuGroup,
+  IconButton,
+} from "@chakra-ui/react";
+import { TiThMenu } from "react-icons/ti";
 
 export default function Hamburger() {
-
   return (
-    <IconButton
-      aria-label="Menu"
-      variant="ghost"
-      icon={<AiOutlineEllipsis />}
-    />
+    <Menu isLazy>
+      <MenuButton
+        as={IconButton}
+        aria-label="Information Menu"
+        icon={<TiThMenu />}
+        variant="ghost"
+        transition="all 0.2s"
+        _hover={{ bg: "kali.400" }}
+        _expanded={{ bg: "kali.400" }}
+      />
+      <MenuList>
+        <MenuGroup title="General">
+          <MenuItem _focus={{ bg: "kali.700" }}>My DAOs</MenuItem>
+          <MenuItem>Tools</MenuItem>
+          {/*TODO: Add Tools Sub-Menu*/}
+        </MenuGroup>
+        <MenuGroup title="Help">
+          <MenuItem _focus={{ bg: "kali.700" }}>FAQs</MenuItem>
+          <MenuItem _focus={{ bg: "kali.700" }}>Docs</MenuItem>
+          <MenuItem _focus={{ bg: "kali.700" }}>Support</MenuItem>
+        </MenuGroup>
+      </MenuList>
+    </Menu>
   );
 }

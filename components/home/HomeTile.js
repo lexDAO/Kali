6c4db31d579770458969702f52bd26b7f0b0331c
Jordan Teague <ejteague@gmail.com>
2021-12-11 14:07:26 -0500

    Update HomeTile.js

--- components/home/HomeTile.js
+++ components/home/HomeTile.js
@@ -4,19 +4,13 @@ import {
   Stack,
 } from "@chakra-ui/react";
 import FlexGradient from '../elements/FlexGradient';
-import {BrowserView, MobileView, isBrowser} from 'react-device-detect';
 
 export default function HomeTile() {
   return(
     <FlexGradient>
       <Stack spacing={5} p={5} alignItems="center">
         <Text fontSize="xl">
-          <BrowserView>
           Launch and automate your business with real DAO magick
-          </BrowserView>
-          <MobileView>
-          MobileView
-          </MobileView>
         </Text>
       </Stack>
     </FlexGradient>

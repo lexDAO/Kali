24d5b6379a9c89c9b2d99f01d9ccedc561487f56
Jordan Teague <ejteague@gmail.com>
2021-12-11 10:47:53 -0500

    mobile styles

--- components/structure/Kali.js
+++ components/structure/Kali.js
@@ -1,8 +1,8 @@
-import {BrowserView, MobileView} from 'react-device-detect';
+import {BrowserView, MobileView, isBrowser} from 'react-device-detect';
 import {
   Box,
   Link,
-  Text
+  IconButton
 } from "@chakra-ui/react";
 import {
   AiFillHome
@@ -10,27 +10,30 @@ import {
 import { routeHome } from '../../utils/router';
 
 export default function Kali() {
-
   const home = () => {
     routeHome();
+    console.log("click")
   }
-
   return(
     <>
+    {isBrowser==true ?
     <Box
       as="h1"
       letterSpacing="wide"
       fontWeight="extrabold"
+      fontSize="4xl"
       bgGradient="linear(to-br, kali.900, kali.600)"
       bgClip="text"
       textShadow="2.4px 0.4px kali.900"
       ml={2}
     >
-      <Link onClick={home}>
-        <BrowserView><Text fontSize="4xl">KaliDAO</Text></BrowserView>
-        <MobileView><Text fontSize="lg">Kali</Text></MobileView>
-      </Link>
+      <Link onClick={home}>KaliDAO</Link>
     </Box>
+    :
+      <Box>
+        <IconButton aria-label='home' icon={<AiFillHome />} onClick={home} />
+      </Box>
+    }
     </>
   )
 }

e6bdaf76165aa40bd2576f3194d4548d60b3c8a3
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-12-12 22:38:30 -0500

    Update addresses.js
    update factory

--- utils/addresses.js
+++ utils/addresses.js
@@ -1,7 +1,7 @@
 // factory addresses
 export const factory_arbitrum = "0xd53B46aE3781904F1f61CF38Fd9d4F47A7e9242B";
 export const factory_polygon = "0x582eAF6a83E55d60615A5FfB80913bE5c1724c41";
-export const factory_rinkeby = "0xD9440D36A70F75F975148778548d0f3e2d791875";
+export const factory_rinkeby = "0xD1B857C5B691b710b39E2e072BCB492F0b13D65d";
 
 // extension addresses
 // - tribute

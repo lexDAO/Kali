1320ec0904aad70e28eb7e0382475049294f44ab
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-12-20 00:00:43 -0500

    Update addresses.js
    redeploy tribute

--- utils/addresses.js
+++ utils/addresses.js
@@ -5,11 +5,11 @@ export const factory_rinkeby = "0x1405101a6959a2105d659bA13c58609eA6E9FEeb";
 
 // extension addresses
 // - tribute
-export const tribute_rinkeby = "0xd024Bf8bb77A5bf3421f1400D408e7C06bbe07ff";
+export const tribute_rinkeby = "0xE490B0cF7feE153d93Aa200E9Ff7021aBe3df346";
 
 export const extensions = { // grouped by chain id
   4: {
-    "tribute": "0xd024Bf8bb77A5bf3421f1400D408e7C06bbe07ff",
+    "tribute": "0xE490B0cF7feE153d93Aa200E9Ff7021aBe3df346",
     "crowdsale": "0x431faBDc0c8B66eE3E19E2F26EC7b640a4E217cD",
     "redemption": "0x43709960A5b35d40B0C0836162B85e88b03FE3a5"
   }

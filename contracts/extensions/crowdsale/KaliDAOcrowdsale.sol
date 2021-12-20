be06e314706d6c9563f1894ad07fc19de9f6922c
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-12-20 00:38:53 -0500

    Update KaliDAOcrowdsale.sol
    error msgs and events

--- contracts/extensions/crowdsale/KaliDAOcrowdsale.sol
+++ contracts/extensions/crowdsale/KaliDAOcrowdsale.sol
@@ -9,6 +9,18 @@ import '../../utils/ReentrancyGuard.sol';
 /// @notice Crowdsale contract that receives ETH or tokens to mint registered DAO tokens, including merkle whitelisting.
 contract KaliDAOcrowdsale is ReentrancyGuard {
     using SafeTransferLib for address;
+
+    event ExtensionSet(uint256 listId, address purchaseToken, uint8 purchaseMultiplier, uint96 purchaseLimit, uint32 saleEnds);
+
+    event ExtensionCalled(address indexed dao, address indexed member, uint256 indexed amountOut);
+
+    error NullMultiplier();
+
+    error SaleEnded();
+
+    error NotWhitelisted();
+
+    error PurchaseLimit();
     
     IKaliWhitelistManager public immutable whitelistManager;
 
@@ -31,7 +43,7 @@ contract KaliDAOcrowdsale is ReentrancyGuard {
         (uint256 listId, address purchaseToken, uint8 purchaseMultiplier, uint96 purchaseLimit, uint32 saleEnds) 
             = abi.decode(extensionData, (uint256, address, uint8, uint96, uint32));
         
-        require(purchaseMultiplier != 0, 'NULL_MULTIPLIER'); 
+        if (purchaseMultiplier == 0) revert NullMultiplier();
 
         crowdsales[msg.sender] = Crowdsale({
             listId: listId,
@@ -41,6 +53,8 @@ contract KaliDAOcrowdsale is ReentrancyGuard {
             amountPurchased: 0,
             saleEnds: saleEnds
         });
+
+        emit ExtensionSet(listId, purchaseToken, purchaseMultiplier, purchaseLimit, saleEnds);
     }
 
     function callExtension(
@@ -50,29 +64,31 @@ contract KaliDAOcrowdsale is ReentrancyGuard {
     ) public payable nonReentrant virtual returns (uint256 amountOut) {
         Crowdsale storage sale = crowdsales[msg.sender];
 
-        require(block.timestamp <= sale.saleEnds, 'SALE_ENDED');
+        if (block.timestamp > sale.saleEnds) revert SaleEnded();
 
-        if (sale.listId != 0) require(whitelistManager.whitelistedAccounts(sale.listId, account), 
-            'NOT_WHITELISTED');
+        if (sale.listId != 0) 
+            if (!whitelistManager.whitelistedAccounts(sale.listId, account)) revert NotWhitelisted();
 
         if (sale.purchaseToken == address(0)) {
             amountOut = msg.value * sale.purchaseMultiplier;
 
-            require(sale.amountPurchased + amountOut <= sale.purchaseLimit, 'PURCHASE_LIMIT');
+            if (sale.amountPurchased + amountOut > sale.purchaseLimit) revert PurchaseLimit();
 
             // send ETH to DAO
-            msg.sender.safeTransferETH(msg.value);
+            msg.sender._safeTransferETH(msg.value);
 
             sale.amountPurchased += uint96(amountOut);
         } else {
             // send tokens to DAO
-            sale.purchaseToken.safeTransferFrom(account, msg.sender, amount);
+            sale.purchaseToken._safeTransferFrom(account, msg.sender, amount);
 
             amountOut = amount * sale.purchaseMultiplier;
 
-            require(sale.amountPurchased + amountOut <= sale.purchaseLimit, 'PURCHASE_LIMIT');
+            if (sale.amountPurchased + amountOut > sale.purchaseLimit) revert PurchaseLimit();
 
             sale.amountPurchased += uint96(amountOut);
         }
+
+        emit ExtensionCalled(msg.sender, account, amountOut);
     }
 }

223cd2ae99a90b7414f6870cfc4cb9755588a3dd
Jordan Teague <ejteague@gmail.com>
2021-12-25 16:57:36 -0500

    added proposal array capability for tokens and kick members

--- components/newproposal/RemoveMember.js
+++ components/newproposal/RemoveMember.js
@@ -1,40 +1,68 @@
 import { useState, useContext, useEffect } from "react";
 import Router, { useRouter } from "next/router";
 import AppContext from "../../context/AppContext";
-import { Input, Button, Select, Text, Textarea, Stack } from "@chakra-ui/react";
+import { Input, Button, Select, Text, Textarea, Stack, List, ListItem, FormControl, FormLabel } from "@chakra-ui/react";
+import { useForm, Controller, useFieldArray } from "react-hook-form";
 import NumInputField from "../elements/NumInputField";
 
 export default function SendShares() {
   const value = useContext(AppContext);
   const { web3, loading, account, abi, address } = value.state;
 
-  const submitProposal = async (event) => {
+  const {
+    handleSubmit,
+    register,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "members",
+  });
+
+  useEffect(() => {
+    append({ address: "" }); // add first member input field
+  }, []);
+
+  const submitProposal = async (values) => {
     event.preventDefault();
     value.setLoading(true);
 
     try {
-      let object = event.target;
-      var array = [];
-      for (let i = 0; i < object.length; i++) {
-        array[object[i].name] = object[i].value;
+
+      var { description_, members } = values; // this must contain any inputs from custom forms
+
+      let accounts_ = [];
+      for (let i = 0; i < members.length; i++) {
+        accounts_.push(members[i].address);
       }
+      console.log("Voters Array", accounts_);
 
-      var { description_, account_, proposalType_ } = array; // this must contain any inputs from custom forms
-
-      const payload_ = Array(0);
+      const proposalType_ = 1;
 
       const instance = new web3.eth.Contract(abi, address);
 
-      const amount_ = await instance.methods.balanceOf(account_).call();
+      let amounts_ = [];
+      for (let i = 0; i < members.length; i++) {
+        const amount_ = await instance.methods.balanceOf(members[i].address).call();
+        amounts_.push(amount_);
+      }
+      console.log("Shares Array", amounts_);
+
+      let payloads_ = [];
+      for (let i = 0; i < accounts_.length; i++) {
+        payloads_.push("0x");
+      }
 
       try {
         let result = await instance.methods
           .propose(
             proposalType_,
             description_,
-            [account_],
-            [amount_],
-            [payload_]
+            accounts_,
+            amounts_,
+            payloads_
           )
           .send({ from: account });
         value.setVisibleView(1);
@@ -51,20 +79,62 @@ export default function SendShares() {
   };
 
   return (
-    <form onSubmit={submitProposal}>
+    <form onSubmit={handleSubmit(submitProposal)}>
       <Stack>
-        <Text>
-          <b>Details</b>
-        </Text>
+      <Controller
+        name="description_"
+        control={control}
+        render={({ field }) => (
+          <FormControl>
+            <FormLabel htmlFor="description_">Description</FormLabel>
+            <Textarea
+              placeholder="0x address or ENS"
+              {...field}
+              {...register(`description_`, {
+                required: "Please enter a description.",
+              })}
+            />
+          </FormControl>
+        )}
+      />
 
-        <Textarea name="description_" size="lg" placeholder=". . ." />
+      <List spacing={2}>
+        {fields.map((member, index) => (
+          <ListItem
+            display="flex"
+            flexDirection="row"
+            alignContent="center"
+            justifyContent="center"
+            key={member.id}
+          >
+            <Controller
+              name={`members.${index}.address`}
+              control={control}
+              defaultValue={member.address}
+              render={({ field }) => (
+                <FormControl>
+                  <FormLabel htmlFor={`members.${index}.address`}>
+                    member {index + 1}
+                  </FormLabel>
+                  <Input
+                    placeholder="0x address or ENS"
+                    {...field}
+                    {...register(`members.${index}.address`, {
+                      required: "You must assign share!",
+                    })}
+                  />
+                </FormControl>
+              )}
+            />
 
-        <Text>
-          <b>Address to Kick</b>
-        </Text>
-        <Input name="account_" size="lg" placeholder="0x or .eth"></Input>
+            <Button variant="ghost" onClick={() => remove(index)}>
+              X
+            </Button>
+          </ListItem>
+        ))}
+      </List>
 
-        <Input type="hidden" name="proposalType_" value="1" />
+      <Button onClick={() => append({ address: "" })}>Add Member</Button>
 
         <Button type="submit">Submit Proposal</Button>
       </Stack>
--- components/newproposal/SendToken.js
+++ components/newproposal/SendToken.js
@@ -1,58 +1,97 @@
 import { useState, useContext, useEffect } from "react";
 import Router, { useRouter } from "next/router";
 import AppContext from "../../context/AppContext";
-import { Input, Button, Select, Text, Textarea, Stack } from "@chakra-ui/react";
+import { Input, Button, Select, Text, Textarea, Stack, List, ListItem, FormControl, FormLabel } from "@chakra-ui/react";
 import NumInputField from "../elements/NumInputField";
+import { useForm, Controller, useFieldArray } from "react-hook-form";
 import { toDecimals } from "../../utils/formatters";
 import { tokens } from "../../constants/tokens";
 
 export default function SendToken() {
   const value = useContext(AppContext);
   const { web3, loading, account, abi, address, dao } = value.state;
+  const [selectedOptions, setSelectedOptions] = useState([]);
 
-  const submitProposal = async (event) => {
-    event.preventDefault();
+  const handleSelect = (select) => {
+    let value = select.target.value;
+    let id = select.target.id;
+    var array = selectedOptions;
+    array[id] = value;
+    setSelectedOptions(array);
+    console.log(array);
+  }
+
+  const {
+    handleSubmit,
+    register,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "recipients",
+  });
+
+  useEffect(() => {
+    append({ address: "" }); // add first recipient input field
+  }, []);
+
+  const submitProposal = async (values) => {
+    //event.preventDefault();
     value.setLoading(true);
-
+    console.log("event", event)
     try {
-      let object = event.target;
-      var array = [];
-      for (let i = 0; i < object.length; i++) {
-        array[object[i].name] = object[i].value;
-      }
 
       var {
-        proposalType_,
         description_,
-        tokenIndex_,
-        amount_,
-        recipient_,
-        tokenAmount_,
-      } = array; // this must contain any inputs from custom forms
+        recipients
+      } = values; // this must contain any inputs from custom forms
+      console.log("values", values)
 
-      console.log(array);
-      let account_ = tokens[tokenIndex_]["address"];
+      const proposalType_ = 2;
 
-      let decimals = tokens[tokenIndex_]["decimals"];
+      let amounts_ = [];
+      for (let i = 0; i < recipients.length; i++) {
+        amounts_.push(0);
+      }
+      console.log("Amounts Array", amounts_);
 
-      tokenAmount_ = toDecimals(tokenAmount_, decimals).toString();
+      // voters ENS check
+      let accounts_ = [];
+      for (let i = 0; i < recipients.length; i++) {
+        let tokenIndex = selectedOptions[i];
+        let address_ = tokens[tokenIndex]["address"];
+        accounts_.push(address_);
+      }
+      console.log("Tokens Array", accounts_);
+
+      let payloads_ = [];
+      for (let i = 0; i < recipients.length; i++) {
+        const ierc20 = require("../../abi/ERC20.json");
+        let tokenIndex = selectedOptions[i];
+        let address_ = tokens[tokenIndex]["address"];
+        let decimals = tokens[tokenIndex]["decimals"];
+        let amt = toDecimals(recipients[i].share, decimals).toString();
+        const tokenContract = new web3.eth.Contract(ierc20, address_);
+        var payload_ = tokenContract.methods
+          .transfer(recipients[i].address, amt)
+          .encodeABI();
+        payloads_.push(payload_);
+      }
+
+      console.log(payloads_);
 
       const instance = new web3.eth.Contract(abi, address);
-
-      const ierc20 = require("../../abi/ERC20.json");
-      const tokenContract = new web3.eth.Contract(ierc20, account_);
-      var payload_ = tokenContract.methods
-        .transfer(recipient_, tokenAmount_)
-        .encodeABI();
 
       try {
         let result = await instance.methods
           .propose(
             proposalType_,
             description_,
-            [account_],
-            [amount_],
-            [payload_]
+            accounts_,
+            amounts_,
+            payloads_
           )
           .send({ from: account });
         value.setVisibleView(1);
@@ -69,35 +108,102 @@ export default function SendToken() {
   };
 
   return (
-    <form onSubmit={submitProposal}>
+    <form onSubmit={handleSubmit(submitProposal)}>
       <Stack>
-        <Text>
-          <b>Details</b>
-        </Text>
-        <Textarea name="description_" size="lg" placeholder=". . ." />
-        <Text>
-          <b>Recipient</b>
-        </Text>
-        <Input name="recipient_" size="lg" placeholder="0x or .eth"></Input>
-        <Text>
-          <b>Token</b>
-        </Text>
 
-        <Select name="tokenIndex_">
-          {dao["balances"].map((b, index) => (
-            <option key={index} value={index}>
-              {b["token"]}
-            </option>
-          ))}
-        </Select>
-        <Text>
-          <b>Amount</b>
-        </Text>
-        <NumInputField name="tokenAmount_" />
+      <Controller
+        name="description_"
+        control={control}
+        render={({ field }) => (
+          <FormControl>
+            <FormLabel htmlFor="description_">Description</FormLabel>
+            <Textarea
+              placeholder="0x address or ENS"
+              {...field}
+              {...register(`description_`, {
+                required: "Please enter a description.",
+              })}
+            />
+          </FormControl>
+        )}
+      />
 
-        <Input type="hidden" name="proposalType_" value="2" />
+      <List spacing={2}>
+        {fields.map((recipient, index) => (
+          <ListItem
+            display="flex"
+            flexDirection="row"
+            alignContent="center"
+            justifyContent="center"
+            key={recipient.id}
+          >
+            <Controller
+              name={`recipients.${index}.address`}
+              control={control}
+              defaultValue={recipient.address}
+              render={({ field }) => (
+                <FormControl>
+                  <FormLabel htmlFor={`recipients.${index}.address`}>
+                    Recipient {index + 1}
+                  </FormLabel>
+                  <Input
+                    placeholder="0x address or ENS"
+                    {...field}
+                    {...register(`recipients.${index}.address`, {
+                      required: "You must assign share!",
+                    })}
+                  />
+                </FormControl>
+              )}
+            />
+            <Controller
+              name={`recipients.${index}.token`}
+              control={control}
+              defaultValue={recipient.token}
+              render={({ field }) => (
+                <FormControl>
+                  <FormLabel htmlFor={`recipients.${index}.token`}>
+                    Token
+                  </FormLabel>
+                  <Select id={index} onChange={handleSelect}>
+                      <option>Select a token</option>
+                    {dao["balances"].map((b, index) => (
+                      <option key={index} value={index}>
+                        {b["token"]}
+                      </option>
+                    ))}
+                  </Select>
+                </FormControl>
+              )}
+            />
+            <Controller
+              name={`recipients.${index}.share`}
+              control={control}
+              defaultValue={recipient.share}
+              render={({ field }) => (
+                <FormControl>
+                  <FormLabel htmlFor={`recipients.${index}.share`}>
+                    Share
+                  </FormLabel>
+                  <Input
+                    placeholder="1"
+                    {...field}
+                    {...register(`recipients.${index}.share`, {
+                      required: "You must assign share!",
+                    })}
+                  />
+                </FormControl>
+              )}
+            />
 
-        <Input type="hidden" name="amount_" value="0" />
+            <Button variant="ghost" onClick={() => remove(index)}>
+              X
+            </Button>
+          </ListItem>
+        ))}
+      </List>
+
+      <Button onClick={() => append({ address: "" })}>Add Recipient</Button>
 
         <Button type="submit">Submit Proposal</Button>
       </Stack>

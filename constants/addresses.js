a391a4f6f86406afc2ecb349f2917ffb8f79aa7b
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-12-26 15:35:44 -0500

    Update addresses.js
    bump crowdsale

--- constants/addresses.js
+++ constants/addresses.js
@@ -6,7 +6,7 @@ export const addresses =
       factory: "0x1405101a6959a2105d659bA13c58609eA6E9FEeb",
       extensions: {
         tribute: "0x0b7012ef8ddd92BA381D06659b6Ed88e22cAe047",
-        crowdsale: "0x4ED219Ef77EF8367649496b1D0876BC065ed1ecc",
+        crowdsale: "0x81F24C53Cae4964EDB22E87b050536637642a9C8",
         redemption: "0x4973670f38c98126F4cA1d74A92F7FE397d18F58",
       },
     },

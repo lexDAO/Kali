ecb74ef67c9dea74f8da195af447ecc065fb49c2
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-12-29 22:42:38 -0500

    Update RicardianLLC.sol
    fix imports

--- contracts/RicardianLLC.sol
+++ contracts/RicardianLLC.sol
@@ -2,8 +2,8 @@
 
 pragma solidity >=0.8.4;
 
-import "../tokens/erc721/ERC721.sol";
-import "../utils/Multicall.sol";
+import "./tokens/erc721/ERC721.sol";
+import "./utils/Multicall.sol";
 
 /// @notice Ricardian LLC NFT minter.
 contract RicardianLLC is ERC721, Multicall {

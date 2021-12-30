9e6f156fd9eb43119a11838ae85b2256676dd378
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-12-29 22:12:59 -0500

    Update addresses.js
    bump factory

--- constants/addresses.js
+++ constants/addresses.js
@@ -3,7 +3,7 @@ export const addresses =
 
   {
     4: {
-      factory: "0x1405101a6959a2105d659bA13c58609eA6E9FEeb",
+      factory: "0x4C7129E82AB2D2B7BB40e38212935B25b590378A",
       extensions: {
         tribute: "0x0b7012ef8ddd92BA381D06659b6Ed88e22cAe047",
         crowdsale: "0x81F24C53Cae4964EDB22E87b050536637642a9C8",

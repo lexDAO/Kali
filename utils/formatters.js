56552883c0bb4fe46c565be6483bfe8e00485360
Jordan Teague <ejteague@gmail.com>
2022-01-02 21:12:48 -0500

    part of toDecimals somehow got deleted, fixed it

--- utils/formatters.js
+++ utils/formatters.js
@@ -48,10 +48,16 @@ export function votingPeriodToSeconds(period, type) {
 
 export function toDecimals(amount, decimals) {
   // this methodology is necessary to avoid javascript autoconverting large numbers to scientific notation
-  var number = amount.toString();
-  for(var i=0; i < decimals; i++) {
-    number += "0";
+  var number = 0;
+  if(amount < 1) {
+    number = amount * scientificNotation[decimals];
+  } else {
+    number = amount.toString();
+    for(var i=0; i < decimals; i++) {
+      number += "0";
+    }
   }
+
   return number;
 }
 

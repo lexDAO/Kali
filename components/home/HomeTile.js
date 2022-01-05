0f202c64514ea634fc2ecaedaf916b87e84ad729
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-01-05 18:53:57 -0500

    Update HomeTile.js
    ✍️ fill out home tile text

--- components/home/HomeTile.js
+++ components/home/HomeTile.js
@@ -9,11 +9,11 @@ export default function HomeTile(props) {
         <Box size="lg"><Image src='https://i.pinimg.com/564x/ba/3f/f9/ba3ff9c3fef67713d3e6b8141a7e3155.jpg' /></Box>
         <Stack>
           <Text fontSize="xl">
-            Launch and automate your business with real DAO magick.
+            Launch a limited liability DAO.
           </Text>
-          <Text>Lorem ipsum dolor sit amet, sea persius accumsan id.</Text>
-          <Text>Veniam primis indoctum ne mei, ne corpora recusabo usu, postea tincidunt te sed.</Text>
-          <Button onClick={props.setDeployerVisible}>Create your DAO</Button>
+          <Text>Select from vetted templates for a club, fund or company.</Text>
+          <Text>Shares will be tokenized and group decisions will be automatically executed.</Text>
+          <Button onClick={props.setDeployerVisible}>Create your LAO</Button>
           {/* <ContactForm /> */}
         </Stack>
       </HStack>

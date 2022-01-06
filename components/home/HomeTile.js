658d5ad73fc3e1a2504cd3ca80bfb2c421eba9ff
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-01-05 19:04:03 -0500

    Update HomeTile.js
    simplify

--- components/home/HomeTile.js
+++ components/home/HomeTile.js
@@ -9,11 +9,9 @@ export default function HomeTile(props) {
         <Box size="lg"><Image src='https://i.pinimg.com/564x/ba/3f/f9/ba3ff9c3fef67713d3e6b8141a7e3155.jpg' /></Box>
         <Stack>
           <Text fontSize="xl">
-            Launch a limited liability DAO.
+            Launch a limited liability DAO
           </Text>
-          <Text>Select from vetted templates for a club, fund or company.</Text>
-          <Text>Shares will be tokenized and group decisions will be automatically executed.</Text>
-          <Button onClick={props.setDeployerVisible}>Create your LAO</Button>
+          <Button onClick={props.setDeployerVisible}>Begin</Button>
           {/* <ContactForm /> */}
         </Stack>
       </HStack>

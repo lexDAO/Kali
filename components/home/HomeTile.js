913e15f971050faed9c4a4f75dae8e9ea2f7c1fd
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-01-05 20:21:11 -0500

    Update HomeTile.js
    style nit

--- components/home/HomeTile.js
+++ components/home/HomeTile.js
@@ -9,11 +9,12 @@ export default function HomeTile(props) {
         <Box size="lg"><Image src='https://i.pinimg.com/564x/ba/3f/f9/ba3ff9c3fef67713d3e6b8141a7e3155.jpg' /></Box>
         <Stack>
           <Text fontSize="xl">
-            Launch a limited liability DAO.
+            <b>Launch a limited liability DAO</b>
           </Text>
-          <Text>Select from vetted templates for a club, fund or company.</Text>
-          <Text>Shares will be tokenized and group decisions will be automatically executed.</Text>
-          <Button onClick={props.setDeployerVisible}>Create your LAO</Button>
+          <Text fontSize="md">
+            <i>form an LLC or UNA in minutes</i>
+          </Text>
+          <Button onClick={props.setDeployerVisible}>Begin</Button>
           {/* <ContactForm /> */}
         </Stack>
       </HStack>

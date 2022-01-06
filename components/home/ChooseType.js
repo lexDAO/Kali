e7c456289cff08e009a65cfb24adf73d656d9b44
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-01-05 20:38:17 -0500

    Update ChooseType.js
    🥢 nit

--- components/home/ChooseType.js
+++ components/home/ChooseType.js
@@ -1,90 +1,136 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import AppContext from "../../context/AppContext";
-import { Flex, VStack, Button, Text, LinkBox, LinkOverlay, Grid } from "@chakra-ui/react";
+import { FormErrorMessage, FormLabel, FormControl, Input, VStack, Button, Text, List, ListItem, IconButton } from "@chakra-ui/react";
+import { AiOutlineDelete } from "react-icons/ai";
+import { useForm, Controller, useFieldArray } from "react-hook-form";
 import { supportedChains } from "../../constants/supportedChains";
-import { getNetworkName, convertVotingPeriod } from "../../utils/formatters";
-import { presets } from "../../constants/presets";
+import { getNetworkName, toDecimals, fromDecimals } from "../../utils/formatters";
 
-export default function ChooseType(props) {
+export default function ChooseMembers(props) {
   const value = useContext(AppContext);
   const { web3, chainId, loading, account } = value.state;
-  const daoType = props.details['daoType'];
 
-  const handleClick = (id) => {
+  const {
+    handleSubmit,
+    register,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "founders",
+  });
+
+  useEffect(() => {
+    if(props.details['members'] == null) {
+      append({ address: "" });
+    } else {
+      for(let i=0; i < props.details['members'].length; i++) {
+        append({ address: props.details['members'][i], share: fromDecimals(props.details['shares'][i], 18) });
+      }
+    }
+
+  }, []);
+
+  const handleMembersSubmit = async (values) => {
+    console.log("Form: ", values);
+
+    const {
+      founders
+    } = values;
+
+    // convert shares to wei
+    let sharesArray = [];
+    for (let i = 0; i < founders.length; i++) {
+      sharesArray.push(toDecimals(founders[i].share, 18));
+    }
+    console.log("Shares Array", sharesArray);
+
+    let votersArray = [];
+    for (let i = 0; i < founders.length; i++) {
+      if(web3.utils.isAddress(founders[i].address) == false) {
+        value.toast(founders[i].address + " is not a valid Ethereum address.");
+        return;
+      }
+      votersArray.push(founders[i].address);
+    }
+    console.log("Voters Array", votersArray)
+
     let array = props.details;
-    array['daoType'] = id;
-    array['quorum'] = presets[id]['quorum'];
-    array['supermajority'] = presets[id]['supermajority'];
-    array['votingPeriod'] = presets[id]['voting'];
-    array['paused'] = presets[id]['paused'];
-    array['extensions'] = presets[id]['extensions'];
+    array['members'] = votersArray;
+    array['shares'] = sharesArray;
     props.setDetails(array);
     console.log(props.details)
 
-    props.handleNext(4);
-
-  }
-
-  const custom = () => {
-    props.handleNext(3);
-  }
-
-  const DaoBox = (item) => {
-    return(
-      <LinkBox
-        key={item.id}
-        border="1px solid"
-        p={5}
-        m={2}
-        borderRadius="2xl"
-        _hover={{
-          bgGradient: "linear(to-br, kali.600, kali.700)",
-        }}
-      >
-        <LinkOverlay href="#" onClick={() => handleClick(item.id)}>
-          <Text fontSize="xl">{item.type['type']}</Text>
-          <Text>Voting Period: {convertVotingPeriod(item.type['voting'])}</Text>
-          <Text>Quorum: {item.type['quorum']}%</Text>
-          <Text>Supermajority: {item.type['supermajority']}%</Text>
-          <Text>{item.type['paused'] == 0 ? "Shares transferrable" : "Shares nontransferrable"}</Text>
-          <Text>Extras:
-            {Object.entries(item.type['extensions']).map(([key, value]) => (
-              <Text key={key}>{value['description']}</Text>
-            ))}
-          </Text>
-        </LinkOverlay>
-      </LinkBox>
-    )
-  }
+    props.handleNext(5);
+  };
 
   return (
-    <VStack>
-        <Text>What type of DAO would you like to deploy?</Text>
-        <Grid
-          templateColumns={{
-            sm: "repeat(1, 1fr)",
-            md: "repeat(2, 1fr)",
-            lg: "repeat(2, 1fr)",
-          }}
-        >
-        {presets.map((item, index) => (
-          <DaoBox key={index} id={index} type={item} />
+    <VStack as="form" onSubmit={handleSubmit(handleMembersSubmit)}>
+      <Text fontSize="xl"><b>Build your cap table</b></Text>
+      <List spacing={2}>
+        {fields.map((founder, index) => (
+          <ListItem
+            display="flex"
+            flexDirection="row"
+            alignContent="center"
+            justifyContent="center"
+            key={founder.id}
+          >
+            <Controller
+              name={`founders.${index}.address`}
+              control={control}
+              defaultValue={founder.address}
+              render={({ field }) => (
+                <FormControl isRequired>
+                  <FormLabel htmlFor={`founders.${index}.address`}>
+                    Founder
+                  </FormLabel>
+                  <Input
+                    placeholder="0x address or ENS"
+                    {...field}
+                    {...register(`founders.${index}.address`, {
+                      required: "You must assign share!",
+                    })}
+                  />
+                </FormControl>
+              )}
+            />
+            <Controller
+              name={`founders.${index}.share`}
+              control={control}
+              defaultValue={founder.share}
+              render={({ field }) => (
+                <FormControl isRequired>
+                  <FormLabel htmlFor={`founders.${index}.share`}>
+                    Share {index + 1}
+                  </FormLabel>
+                  <Input
+                    placeholder="1"
+                    {...field}
+                    {...register(`founders.${index}.share`, {
+                      required: "You must assign share!",
+                    })}
+                  />
+                </FormControl>
+              )}
+            />
+            <IconButton
+              aria-label="delete founder"
+              isRound
+              variant="ghost"
+              _hover={{ bg: "kali.600" }}
+              mt={8}
+              ml={2}
+              icon={<AiOutlineDelete />}
+              onClick={() => remove(index)}
+            />
+          </ListItem>
         ))}
-        <LinkBox
-          border="1px solid"
-          p={5}
-          m={2}
-          borderRadius="2xl"
-          _hover={{
-            bgGradient: "linear(to-br, kali.600, kali.700)",
-          }}
-        >
-          <LinkOverlay href="#" onClick={custom}>
-            <Text>Custom</Text>
-            <Text>Pick this option if you want to customize your DAO&apos;s settings.</Text>
-          </LinkOverlay>
-        </LinkBox>
-        </Grid>
+      </List>
+      <Button onClick={() => append({ address: "" })}>Add Founder</Button>
+      <Button type="submit">Next</Button>
     </VStack>
   );
 }

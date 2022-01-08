e6c0760106ed9e03068a7f16f7b4d485df7160e5
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-01-07 23:32:18 -0500

    Update addresses.js
    nit addr

--- constants/addresses.js
+++ constants/addresses.js
@@ -6,7 +6,7 @@ export const addresses =
       factory: "0xfc0Fd933D8ef5b7e552E67aCfc4c09711B413f30",
       extensions: {
         tribute: "0x0b7012ef8ddd92BA381D06659b6Ed88e22cAe047",
-        crowdsale: "0x4bB6996Fc83F97C2971188274F4B6BE6a8aDbcc4",
+        crowdsale: "0xaDcbe7525960C91Bd2DaD9c697BB9ec4FB027bf3",
         redemption: "0x4973670f38c98126F4cA1d74A92F7FE397d18F58",
       },
     },

cca1e7ec61fe9d6b8125dd6fd736c7c502ae8706
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-01-08 00:04:02 -0500

    Update addresses.js
    bump

--- constants/addresses.js
+++ constants/addresses.js
@@ -6,7 +6,7 @@ export const addresses =
       factory: "0xfc0Fd933D8ef5b7e552E67aCfc4c09711B413f30",
       extensions: {
         tribute: "0x0b7012ef8ddd92BA381D06659b6Ed88e22cAe047",
-        crowdsale: "0x829dc1EED1B9C84498C87427A0FA6A567AFe9436",
+        crowdsale: "0x4a2E86911Ad13Cd08A99Ba78b4097E66C71696fa",
         redemption: "0x4973670f38c98126F4cA1d74A92F7FE397d18F58",
       },
     },

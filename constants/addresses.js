205f96ceec2e5185ecfba08a96c6edc588fbf1b7
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-01-07 22:15:54 -0500

    Update addresses.js
    try

--- constants/addresses.js
+++ constants/addresses.js
@@ -6,7 +6,7 @@ export const addresses =
       factory: "0xfc0Fd933D8ef5b7e552E67aCfc4c09711B413f30",
       extensions: {
         tribute: "0x0b7012ef8ddd92BA381D06659b6Ed88e22cAe047",
-        crowdsale: "0x31766e8D677AcBd0c0Aed22C9d531FA151f782B5",
+        crowdsale: "0x4bB6996Fc83F97C2971188274F4B6BE6a8aDbcc4",
         redemption: "0x4973670f38c98126F4cA1d74A92F7FE397d18F58",
       },
     },

823f53b9d2970eebfc0f966eefa225bf6654dea5
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-01-08 20:59:29 -0500

    🥋 add Create2 (#163)
    * Update KaliDAOfactory.sol
    
    add create2 / multicall
    
    * Update KaliDAOfactory.json
    
    bump
    
    * Update addresses.js
    
    bump factory
    
    * Update addresses.js
    
    bump
    
    * Update KaliDAOfactory.json
    
    bump
    
    * Update KaliDAOfactory.sol
    
    add custom error

--- constants/addresses.js
+++ constants/addresses.js
@@ -3,7 +3,7 @@ export const addresses =
 
   {
     4: {
-      factory: "0xB5528C1A42ae9Fd863aBFe4A6aefb5A2FD24F7Ae",
+      factory: "0x95a6588B42e58015d0762183a1Cf9ab79c79E8e4",
       extensions: {
         tribute: "0x0b7012ef8ddd92BA381D06659b6Ed88e22cAe047",
         crowdsale: "0x4a2E86911Ad13Cd08A99Ba78b4097E66C71696fa",

85fc3d6f93c28ec99eced92c014f4fccbf303a60
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-01-11 15:01:59 -0500

    Update KaliDAOtoken.sol
    🥢nits

--- contracts/KaliDAOtoken.sol
+++ contracts/KaliDAOtoken.sol
@@ -3,7 +3,7 @@
 pragma solidity >=0.8.4;
 
 /// @notice Modern and gas-optimized ERC-20 + EIP-2612 implementation with COMP-style governance and pausing.
-/// @author Modified from RariCapital (https://github.com/Rari-Capital/solmate/blob/main/src/erc20/ERC20.sol)
+/// @author Modified from Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/erc20/ERC20.sol)
 /// License-Identifier: AGPL-3.0-only
 abstract contract KaliDAOtoken {
     /*///////////////////////////////////////////////////////////////
@@ -82,7 +82,7 @@ abstract contract KaliDAOtoken {
     bool public paused;
 
     bytes32 public constant DELEGATION_TYPEHASH = 
-        keccak256('Delegation(address delegatee,uint256 nonce,uint256 expiry)');
+        keccak256('Delegation(address delegatee,uint256 nonce,uint256 deadline)');
 
     mapping(address => address) internal _delegates;
 

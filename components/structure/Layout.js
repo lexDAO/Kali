a30913c3b75dda16adfb895253609ae8c80df4c5
Jordan Teague <ejteague@gmail.com>
2022-01-10 21:34:04 -0500

    Add files via upload

--- components/structure/Layout.js
+++ components/structure/Layout.js
@@ -11,25 +11,6 @@ export default function Layout(props) {
   const { loading } = value.state;
   return (
     <>
-    <style jsx global>{`
-      body {
-        background: linear-gradient(90deg, #e65170, #3b074e);
-        background: linear-gradient(90deg, #a0b1dc, #594cfb);
-        min-height: 100vh
-      }
-
-        body:before {
-          content: "";
-          position: absolute;
-          top: 0;
-          left: 0;
-          bottom: 0;
-          right: 0;
-          mask-image: linear-gradient(to bottom, transparent, black);
-          background: linear-gradient(90deg, hotpink, rebeccapurple);
-          z-index: -1
-        }
-    `}</style>
       {loading == true ? <LoadingIndicator /> : ""}
       <Head>
         <title>KaliDAO</title>

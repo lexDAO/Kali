cd6a32ba7a79664ce448afb1e4147c4d9b2d7f44
nerderlyne <shivanshityagi@icloud.com>
2022-01-16 03:33:01 +0530

    add redemption

--- components/home/Checkout.js
+++ components/home/Checkout.js
@@ -1,9 +1,21 @@
 import React, { useState, useContext } from "react";
 import Router from "next/router";
 import AppContext from "../../context/AppContext";
-import { Flex, VStack, Button, Text, Select, List, ListItem } from "@chakra-ui/react";
+import {
+  Flex,
+  VStack,
+  Button,
+  Text,
+  Select,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
 import { supportedChains } from "../../constants/supportedChains";
-import { getNetworkName, convertVotingPeriod, fromDecimals } from "../../utils/formatters";
+import {
+  getNetworkName,
+  convertVotingPeriod,
+  fromDecimals,
+} from "../../utils/formatters";
 import { addresses } from "../../constants/addresses";
 import { factoryInstance } from "../../eth/factory";
 import { presets } from "../../constants/presets";
@@ -28,41 +40,60 @@ export default function Checkout(props) {
     }
 
     const {
-        network,
-        daoName,
-        symbol,
-        members,
-        shares,
-        votingPeriod,
-        paused,
-        quorum,
-        supermajority,
-        extensions,
-        docs
+      network,
+      daoName,
+      symbol,
+      members,
+      shares,
+      votingPeriod,
+      paused,
+      quorum,
+      supermajority,
+      extensions,
+      docs,
     } = props.details;
 
-    const govSettings = Array(quorum, supermajority, 1,1,1,1,1,1,1,1,1,1,1);
+    const govSettings = Array(
+      quorum,
+      supermajority,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
+    );
 
     let extensionsArray;
     let extensionsData;
 
-    if(extensions == null) {
+    if (extensions == null) {
       extensionsArray = new Array(0);
       extensionsData = new Array(0);
     } else {
-
       extensionsArray = [];
       extensionsData = [];
 
-      if('tribute' in extensions) {
-        extensionsArray.push(addresses[chainId]['extensions']['tribute']);
+      if ("tribute" in extensions) {
+        extensionsArray.push(addresses[chainId]["extensions"]["tribute"]);
         extensionsData.push("0x");
       }
 
-      if('crowdsale' in extensions) {
-        extensionsArray.push(addresses[chainId]['extensions']['crowdsale']);
+      if ("crowdsale" in extensions) {
+        extensionsArray.push(addresses[chainId]["extensions"]["crowdsale"]);
 
-        var { listId, purchaseToken, purchaseMultiplier, purchaseLimit, saleEnds } = extensions['crowdsale'];
+        var {
+          listId,
+          purchaseToken,
+          purchaseMultiplier,
+          purchaseLimit,
+          saleEnds,
+        } = extensions["crowdsale"];
         let now = parseInt(new Date().getTime() / 1000);
         saleEnds += now;
 
@@ -74,41 +105,61 @@ export default function Checkout(props) {
 
         const encodedParams = web3.eth.abi.encodeParameters(
           ["uint256", "address", "uint8", "uint96", "uint32"],
-          [
-            listId,
-            purchaseToken,
-            purchaseMultiplier,
-            purchaseLimit,
-            saleEnds,
-          ]
+          [listId, purchaseToken, purchaseMultiplier, purchaseLimit, saleEnds]
         );
 
-        var payload = saleContract.methods
+        let payload = saleContract.methods
           .setExtension(encodedParams)
-          .encodeABI(); 
+          .encodeABI();
 
         extensionsData.push(payload);
       }
 
-      if('redemption' in extensions) {
-        extensionsArray.push(addresses[chainId]['extensions']['redemption']);
+      if ("redemption" in extensions) {
+        extensionsArray.push(addresses[chainId]["extensions"]["redemption"]);
 
-        var { redemptionStart, tokenArray } = extensions['redemption'];
+        var { redemptionStart, tokenArray } = extensions["redemption"];
         let now = parseInt(new Date().getTime() / 1000);
         redemptionStart += now;
-        const payload = web3.eth.abi.encodeParameters(
+
+        const redemption = require("../../abi/KaliDAOredemption.json");
+
+        const redemptionAddress =
+          addresses[chainId]["extensions"]["redemption"];
+
+        const redemptionContract = new web3.eth.Contract(
+          redemption,
+          redemptionAddress
+        );
+
+        const encodedParams = web3.eth.abi.encodeParameters(
           ["address[]", "uint256"],
           [tokenArray, redemptionStart]
         );
+
+        let payload = redemptionContract.methods
+          .setExtension(encodedParams)
+          .encodeABI();
+
         extensionsData.push(payload);
       }
-
     }
 
     console.log("extensionsArray", extensionsArray);
     console.log("extensionsData", extensionsData);
 
-    console.log(daoName, symbol, docs, paused, extensionsArray, extensionsData, members, shares, votingPeriod, govSettings);
+    console.log(
+      daoName,
+      symbol,
+      docs,
+      paused,
+      extensionsArray,
+      extensionsData,
+      members,
+      shares,
+      votingPeriod,
+      govSettings
+    );
 
     try {
       let result = await factory.methods
@@ -140,29 +191,57 @@ export default function Checkout(props) {
     }
 
     value.setLoading(false);
-  }
+  };
 
   return (
     <VStack>
       <Text>You have selected:</Text>
       <List>
-          <ListItem>Chain <b>{details['network']}</b></ListItem>
-          <ListItem>Name <b>{details['daoName']}</b></ListItem>
-          <ListItem>Symbol <b>{details['symbol']}</b></ListItem>
-          <ListItem>Type <b>{details['daoType'] == null ? "Custom" : presets[details['daoType']]['type']}</b>
-          </ListItem>
-          <ListItem>Members
-            <List>
-            <b>{details['members'].map((item, index) => (
-                <ListItem key={index}>{item} ({fromDecimals(details['shares'][index], 18)} shares)</ListItem>
-              ))}</b>
-            </List>
-          </ListItem>
-          <ListItem>Voting period <b>{convertVotingPeriod(details['votingPeriod'])}</b></ListItem>
-          <ListItem>Share transerability <b>{details['paused']==1 ? "restricted" : "unrestricted"}</b></ListItem>
-          <ListItem>Quorum <b>{details['quorum']}%</b></ListItem>
-          <ListItem>Supermajority <b>{details['supermajority']}%</b></ListItem>
-          <ListItem>Docs <b>{details['docs']=="" ? "Ricardian" : details['docs']}</b></ListItem>
+        <ListItem>
+          Chain <b>{details["network"]}</b>
+        </ListItem>
+        <ListItem>
+          Name <b>{details["daoName"]}</b>
+        </ListItem>
+        <ListItem>
+          Symbol <b>{details["symbol"]}</b>
+        </ListItem>
+        <ListItem>
+          Type{" "}
+          <b>
+            {details["daoType"] == null
+              ? "Custom"
+              : presets[details["daoType"]]["type"]}
+          </b>
+        </ListItem>
+        <ListItem>
+          Members
+          <List>
+            <b>
+              {details["members"].map((item, index) => (
+                <ListItem key={index}>
+                  {item} ({fromDecimals(details["shares"][index], 18)} shares)
+                </ListItem>
+              ))}
+            </b>
+          </List>
+        </ListItem>
+        <ListItem>
+          Voting period <b>{convertVotingPeriod(details["votingPeriod"])}</b>
+        </ListItem>
+        <ListItem>
+          Share transerability{" "}
+          <b>{details["paused"] == 1 ? "restricted" : "unrestricted"}</b>
+        </ListItem>
+        <ListItem>
+          Quorum <b>{details["quorum"]}%</b>
+        </ListItem>
+        <ListItem>
+          Supermajority <b>{details["supermajority"]}%</b>
+        </ListItem>
+        <ListItem>
+          Docs <b>{details["docs"] == "" ? "Ricardian" : details["docs"]}</b>
+        </ListItem>
       </List>
       <Button onClick={deploy}>Deploy</Button>
     </VStack>
--- constants/presets.js
+++ constants/presets.js
@@ -7,9 +7,9 @@ export const presets = [
     paused: 1,
     extensions: {
       tribute: {
-        description: "Anyone can join by paying tribute."
-      }
-    }
+        description: "Anyone can join by paying tribute.",
+      },
+    },
   },
   {
     type: "Investment Club",
@@ -19,19 +19,21 @@ export const presets = [
     paused: 1,
     extensions: {
       crowdsale: {
-        description: "ETH crowdsale enabled by default for our Dos Commas accredited investor whitelist. 200 shares per ETH. Limit 1,000 shares per wallet. Sale ends in 30 days.",
+        description:
+          "ETH crowdsale enabled by default for our Dos Commas accredited investor whitelist. 200 shares per ETH. Limit 1,000 shares per wallet. Sale ends in 30 days.",
         purchaseToken: "0x0000000000000000000000000000000000000000",
         purchaseMultiplier: 200,
         purchaseLimit: "1000000000000000000000", // 1,000 shares - string, to avoid BigNumber errors
         saleEnds: 2592000, // 30 days from today
-        listId: 0 // CHANGE THIS FOR LIVE DEPLOYMENT!
+        listId: 0, // CHANGE THIS FOR LIVE DEPLOYMENT!
       },
       redemption: {
-        description: "Members may ragequit for their fair share of the DAO's ETH reserves. Redemption option triggered 30 days after launch. DAO may make other tokens eligible for redemption at a later date by proposal.",
+        description:
+          "Members may ragequit for their fair share of the DAO's ETH reserves. Redemption option triggered 30 days after launch. DAO may make other tokens eligible for redemption at a later date by proposal.",
         redemptionStart: 2592000, // 30 days from today
-        tokenArray: ["0x0000000000000000000000000000000000000000"]
-      }
-    }
+        tokenArray: ["0x0000000000000000000000000000000000000000"],
+      },
+    },
   },
   {
     type: "Services Company",
@@ -41,10 +43,11 @@ export const presets = [
     paused: 1,
     extensions: {
       redemption: {
-        description: "Members may ragequit for their fair share of the DAO's ETH reserves. Redemption option triggered 180 days after launch.  DAO may make other tokens eligible for redemption at a later date by proposal.",
+        description:
+          "Members may ragequit for their fair share of the DAO's ETH reserves. Redemption option triggered 180 days after launch.  DAO may make other tokens eligible for redemption at a later date by proposal.",
         redemptionStart: 15552000, // 180 days from today
-        tokenArray: ["0x0000000000000000000000000000000000000000"]
-      }
-    }
-  }
-]
+        tokenArray: ["0x0000000000000000000000000000000000000000"],
+      },
+    },
+  },
+];
